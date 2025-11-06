f3105da99caffa404fd143dbd429f54b6b753eb1
Fode Manca <fomanca@c2r4s11.42porto.com>
2025-11-06 14:49:26 +0000

    chore: update get_next_line.h
    Updated function prototypes and definitions to match the new implementation of get_next_line.

--- get_next_line.h
+++ get_next_line.h
@@ -5,29 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/20 14:59:23 by fomanca           #+#    #+#             */
-/*   Updated: 2025/10/22 18:37:26 by fomanca          ###   ########.fr       */
+/*   Created: 2025/10/31 14:08:19 by fomanca           #+#    #+#             */
+/*   Updated: 2025/11/02 12:46:19 by fomanca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 # include <fcntl.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 100
+#  define BUFFER_SIZE 1
 # endif
 
-// functions prtotypes
-void	fill_str(char *res, char *s1, char *s2);
-char	*get_next_line(int fd);
-
+size_t	ft_strlen(char *s);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(char *str);
-char	*ft_strdup(const char *src);
-char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*ft_extract_line(char *rem);
+char	*ft_read_fd(int fd, char *rem);
+char	*ft_reminder(char *rem);
+char	*get_next_line(int fd);
 
 #endif
